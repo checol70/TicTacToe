5f30a8f2c755218ae75bd7cd4d1d96e8635e7dfa
Sheckerol <checol70@gmail.com>
2020-02-05 00:27:45 -0700

    Added win check and changed board array to ints to better work with win check

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import './App.css';
+import './reset.css';
 import Home from './pages/Home'
 
 function App() {
--- tic-tac-toe/src/pages/Home/Home.js
+++ tic-tac-toe/src/pages/Home/Home.js
@@ -3,33 +3,68 @@ import "./Home.css"
 class Home extends Component{
     state = {
         board:[
-            ["","",""],
-            ["","",""],
-            ["","",""]
-        ]
+            [0,0,0],
+            [0,0,0],
+            [0,0,0]
+        ],
+        decode:["X","","O"],
+        winner:""
     }
     showState = ()=>{
         let table = [];
         this.state.board.forEach((element,rowNum) => {
             let row = [];
             element.forEach((val,itemNum)=>{
-                row.push(<p className = "shower" key = {rowNum.toString() + itemNum.toString()}  onClick = {()=>{if(val ==="")this.makeMove(rowNum,itemNum)}}>{val}</p>)
+                row.push(<p className = "shower" key = {rowNum.toString() + itemNum.toString()}  onClick = {()=>{if(val ===0)this.makeMove(rowNum,itemNum)}}>{"\u00a0"+this.state.decode[val+1]+"\u00a0"}</p>)
             });
             table.push(<div className = "row"key = {rowNum}>{row}</div>);
         })
         return table;
     }
 
+    checkWin = (board)=>{
+        for(let i = 0; i<3; i++)
+        {
+            if(board[i][0] === board[i][1] && board[i][2] === board[i][1] && board[i][0] !== 0)
+            {
+                return board[i][0];
+            } 
+            if(board[0][i] === board[1][i] && board[2][i] === board[1][i] && board[0][i] !== 0)
+            {
+                return board[0][i];
+            }
+        }
+        if(board[0][0] === board[1][1] && board[2][2] === board[1][1] && board[1][1] !== 0)
+        {
+            return board[0][0];
+        }
+        if(board[0][2] === board[1][1] && board[2][0] === board[1][1] && board[1][1] !== 0){
+            return board[0][2];
+        }
+        return 0;
+    }
+
     makeMove = (i,j)=>{
         console.log("Called!")
         let state = this.state;
-        state.board[i][j] = "X";
+        state.board[i][j] = -1;
+        state = this.computerMakeMove(state);
+        let winner = this.checkWin(state.board)
+        if(winner !== 0){
+            state.winner = state.decode[winner + 1]
+        }
         this.setState(state);
         
     }
 
+    computerMakeMove = (state)=>{
+        
+        return state;
+    }
+
     render(){
        return (<div>
+           <p>{this.state.winner}</p>
             {this.showState()}
         </div>)
     }
