a3d026556ac130a9a0bb97f567c5676685ac0fa7
Sheckerol <checol70@gmail.com>
2020-02-08 03:39:55 -0700

    Finished game logic, on to CSS

--- tic-tac-toe/src/pages/Home/Home.js
+++ tic-tac-toe/src/pages/Home/Home.js
@@ -1,115 +1,97 @@
 import React, { Component } from "react";
 import "./Home.css"
-class Home extends Component{
+class Home extends Component {
     state = {
-        board:[
-            [0,0,0],
-            [0,0,0],
-            [0,0,0]
+        board: [
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
         ],
-        decode:["X","","O"],
-        winner:""
+        decode: ["X", "", "O"],
+        winner: ""
     }
-    showState = ()=>{
+    showState = () => {
         let table = [];
-        this.state.board.forEach((element,rowNum) => {
+        this.state.board.forEach((element, rowNum) => {
             let row = [];
-            element.forEach((val,itemNum)=>{
-                row.push(<p className = "shower" key = {rowNum.toString() + itemNum.toString()}  onClick = {()=>{if(val ===0)this.makeMove(rowNum,itemNum)}}>{"\u00a0"+this.state.decode[val+1]+"\u00a0"}</p>)
+            element.forEach((val, itemNum) => {
+                row.push(<p className="shower" key={rowNum.toString() + itemNum.toString()} onClick={() => { if (val === 0) this.makeMove(rowNum, itemNum) }}>{"\u00a0" + this.state.decode[val + 1] + "\u00a0"}</p>)
             });
-            table.push(<div className = "row"key = {rowNum}>{row}</div>);
+            table.push(<div className="row" key={rowNum}>{row}</div>);
         })
         return table;
     }
 
-    checkWin = (board)=>{
-        for(let i = 0; i<3; i++)
-        {
-            if(board[i][0] === board[i][1] && board[i][2] === board[i][1] && board[i][0] !== 0)
-            {
+    checkWin = (board) => {
+        for (let i = 0; i < 3; i++) {
+            if (board[i][0] === board[i][1] && board[i][2] === board[i][1] && board[i][0] !== 0) {
                 return board[i][0];
-            } 
-            if(board[0][i] === board[1][i] && board[2][i] === board[1][i] && board[0][i] !== 0)
-            {
+            }
+            if (board[0][i] === board[1][i] && board[2][i] === board[1][i] && board[0][i] !== 0) {
                 return board[0][i];
             }
         }
-        if(board[0][0] === board[1][1] && board[2][2] === board[1][1] && board[1][1] !== 0)
-        {
+        if (board[0][0] === board[1][1] && board[2][2] === board[1][1] && board[1][1] !== 0) {
             return board[0][0];
         }
-        if(board[0][2] === board[1][1] && board[2][0] === board[1][1] && board[1][1] !== 0){
+        if (board[0][2] === board[1][1] && board[2][0] === board[1][1] && board[1][1] !== 0) {
             return board[0][2];
         }
         return 0;
     }
 
-    makeMove = (i,j)=>{
+    makeMove = (i, j) => {
         let state = this.state;
         state.board[i][j] = -1;
+        
         let winner = this.checkWin(state.board);
-        if(winner === 0){
-            state = this.computerMakeMove(state);
-            winner = this.checkWin(state.board);
+        let draw = state.board[0].concat(state.board[1]).concat(state.board[2]).indexOf(0) === -1
+        if (winner === 0 && !draw) {
+                state = this.computerMakeMove(state);
+                winner = this.checkWin(state.board);
+        }else if(draw){
+            state.winner = "Stalemate"
         }else{
             state.winner = state.decode[winner + 1];
         }
         this.setState(state);
-        
     }
 
-    getPossibleMoves = (board, value)=>{
-        let possibleMoves =[];
-        board.forEach((row,rowNum)=>{
-            row.forEach((value, valueNum)=>{
-                if(value === 0)
-                possibleMoves.push([rowNum,valueNum])
+    getPossibleMoves = (board, value) => {
+        let possibleMoves = [];
+        board.forEach((row, rowNum) => {
+            row.forEach((value, valueNum) => {
+                if (value === 0)
+                    possibleMoves.push([rowNum, valueNum])
             })
         })
         const possibleBoardStates = []
-        possibleMoves.forEach(e=>{
+        possibleMoves.forEach(e => {
             let tempBoard = JSON.parse(JSON.stringify(board));
             possibleBoardStates.push(tempBoard);
         })
-        possibleBoardStates.forEach((e,i)=>{
+        possibleBoardStates.forEach((e, i) => {
             e[possibleMoves[i][0]][possibleMoves[i][1]] = value;
         })
         return possibleBoardStates;
     }
 
-    test=(board, recursionLevel,previousLevel, minMax)=>{
-        let functions = [
-            (arr)=>Math.min(...arr),
-            (arr)=>Math.max(...arr)
-        ]
-        let possibles = this.getPossibleMoves(board,recursionLevel%2!==1? 1:-1 );
-        let chances = possibles.map(e=>this.checkWin(e));
-        let currentLevel = {possibles,
+    test = (board, recursionLevel, previousLevel, minMax) => {
+        let possibles = this.getPossibleMoves(board, recursionLevel % 2 !== 1 ? 1 : -1);
+        let chances = possibles.map(e => this.checkWin(e));
+        let currentLevel = {
+            possibles,
             chances,
-            likelyResult : 0
-        }
-        currentLevel.likelyResult = functions[recursionLevel%2](chances);
-        
-        // console.log("CurrentLevel: ");
-        // console.log(currentLevel);
-        const twoMoves = []
-        //console.log(recursionLevel)
-        if(recursionLevel<5 && currentLevel.likelyResult === 0){
+            recursionLevel
+        };
+        const twoMoves = [];
+        if (recursionLevel < 3 && !currentLevel.chances.includes(-1)) {
             recursionLevel++;
-            for(let i = 0; i<possibles.length; i++){
-                if(chances[i] === 0){
-                    const check = this.test(possibles[i], Math.parse(recursionLevel.toString()), currentLevel)
-                    if(check !== undefined && check.likelyResult === -1){
-                        currentLevel.likelyResult = -1;
-                        return currentLevel;
-                    }
-                    twoMoves.push(check)}
-                //console.log("here I am.")
-                if(twoMoves[i] !== undefined){ 
-                    if(twoMoves[i].chances.includes(-1)){
-                        //currentLevel.likelyResult = twoMoves[i].likelyResult;
-                        break;
-                    }
+            for (let i = 0; i < possibles.length; i++) {
+                if (chances[i] === 0) {
+                    const check = this.test(possibles[i], parseInt(recursionLevel.toString()), currentLevel)
+                    
+                    twoMoves.push(check)
                 }
             }
         }
@@ -117,35 +99,57 @@ class Home extends Component{
         return currentLevel
     }
 
-    getDifferentLevel(twoMoves){
-        twoMoves.forEach((e,i)=>{
-            if(!e.chances.includes(-1) && e.chances.includes(1)){
+    getDifferentLevel(twoMoves) {
+        twoMoves.forEach((e, i) => {
+            if (!e.chances.includes(-1) && e.chances.includes(1)) {
                 return i;
             }
         })
         return 0;
     }
 
-    getIdealChoice(results){
-        results.chances.forEach((e,i)=>{
-            if(e>0){
-                return(results.possibleMoves[i])
-            }
+    getIdealChoice(results) {
+        const minResults = results.twoMoves.map((move)=>{
+            return Math.min(...move.chances);
         })
-
+        let correctChoice = minResults.indexOf(Math.max(...minResults));
+        return results.possibles[correctChoice];
     }
-    
-    computerMakeMove = (state)=>{
-        let results = this.test(state.board,0,null);
-        console.log("results")
-        console.log(results);
-        console.log(this.getIdealChoice(results))
+
+    computerMakeMove = (state) => {
+        //first we check to see if we are in the first turn, since this will simplify the number of calculations, and guarantee the correct first choice.
+        const filledPlaces = state.board.map(row => row.map(value => Math.abs(value)));
+        const totalMoves = filledPlaces.reduce((a, b) => a + b.reduce((c, d) => c + d, 0), 0);
+        if (totalMoves === 1) {
+            if (state.board !== [[0, 0, 0], [0, -1, 0], [0, 0, 0]]) {
+                state.board[1][1] = 1;
+                return state;
+            }
+            const corner = Math.floor(Math.random() * 4);
+            switch (corner) {
+                case 0:
+                    state.board[0][0] = 1;
+                    break;
+                case 1:
+                    state.board[2][0] = 1;
+                    break;
+                case 2:
+                    state.board[0][2] = 1;
+                    break;
+                default:
+                    state.board[2][2] = 1;
+                    break;
+            }
+            return state;
+        }
+        let results = this.test(state.board, 0, null);
+        state.board = this.getIdealChoice(results)
         return state;//Math.max(results);
     }
 
-    render(){
-       return (<div>
-           <p>{this.state.winner}</p>
+    render() {
+        return (<div>
+            <p>{this.state.winner}</p>
             {this.showState()}
         </div>)
     }
